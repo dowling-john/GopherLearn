2d29949c2c959e1ba173b2f8e9de428f90b9181d
Flexin1981 <7731996+Flexin1981@users.noreply.github.com>
2024-08-02 10:03:46 +0100

    adding a neuron initialization function adding a layer creation factory

--- gopher_learn/neural_network/layer.go
+++ gopher_learn/neural_network/layer.go
@@ -6,6 +6,14 @@ type (
 	}
 )
 
+func NewFullyConnectedLayer(number_of_neurons int, activation_func func(float64) float64) *FullyConnectedLayer {
+	var neurons []*Neuron
+	for i := 0; i < number_of_neurons; i++ {
+		neurons = append(neurons, &Neuron{Activation: activation_func})
+	}
+	return &FullyConnectedLayer{Neurons: neurons}
+}
+
 func (f *FullyConnectedLayer) Forward(inputs []float64) (NeuronOutputs []float64) {
 	for _, neuron := range f.Neurons {
 		NeuronOutputs = append(NeuronOutputs, neuron.Forward(inputs))
--- gopher_learn/neural_network/nuron_test.go
+++
@@ -1,1 +0,0 @@
-package neural_network
